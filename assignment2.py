efe705a5812cad8ed50f07c71f9558d37c2acf92
Shree24 <smailybhatnagar@gmail.com>
2018-06-14 10:10:33 +0530

    assignment2

---
+++ assignment2.py
@@ -0,0 +1,36 @@
+#Question1
+print("hello world")
+#end
+#Question2
+a="Acad"
+b="view"
+print(a+b)
+#end
+#Question3
+print('let\'s get started')
+
+#Question 4
+x=input("enter no1")
+y=input("enter no2")
+z=input("enter no3")
+print(x)
+print(y)
+print(z)
+#end
+#question 5
+s="Acadview"
+course="Python"
+fees=5000
+print("welcome to %s your course is %s and its fees is %d"%(s,course,fees))
+#question 5
+s="Acadview"
+course="Python"
+fees=5000
+print("welcome to %s your course is %s and its fees is %d"%(s,course,fees))
+#end
+
+#question 6
+name="Tonny stark"
+salary=1000000
+print('%s %d'%(name,salary))
+#end
